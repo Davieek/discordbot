4e7d91f62cd9375b338402a0396798e5e77a74bd
Davieek <90272349+Davieek@users.noreply.github.com>
2021-09-10 06:29:03 +0200

    Delete src/commands directory

--- src/commands/avatar.cmd.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-module.exports = {
-    name : 'avatar',
-    execute(client, message, args){
-        let member = message.mentions.users.first() || message.author;
-        message.channel.send( new Discord.MessageEmbed() .setColor("RANDOM") .setImage(member.displayAvatarURL()))
-    }
-}
